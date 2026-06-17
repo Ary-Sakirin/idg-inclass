65e3595da0d3a0cd462c0c8fb8c3c10d0bd21343
Ary-Sakirin <arysakirin29@gmail.com>
2026-06-17 15:52:26 +0700

    Creating a Students Listing Page

--- src/components/includes/Sidebar.vue
+++ src/components/includes/Sidebar.vue
@@ -51,6 +51,16 @@
           <li class="nav-header">Academic Management</li>
           <li class="nav-item">
             <router-link
+              :to="{ name: 'Students' }"
+              active-class="active"
+              class="nav-link"
+            >
+              <i class="nav-icon fas fa-user-graduate"></i>
+              <p>Students</p>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link
               :to="{ name: 'Tests' }"
               active-class="active"
               class="nav-link"
